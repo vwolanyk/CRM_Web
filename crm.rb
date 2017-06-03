6b5fde8e7801d52c67547e1d03f2aa489b3630f2
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-03 17:31:58 -0400

    Clean up comments and indentation

--- crm.rb
+++ crm.rb
@@ -1,38 +1,54 @@
 require_relative 'contact'
 require 'sinatra'
+require 'sinatra/reloader'
 
+# HOME PAGE
 
 get "/" do
 
-  erb :index
+  redirect to('/index')
 
 end
+
+# INDEX AKA HOME
 
 get "/index" do
 
   erb :index
 end
 
+# ALL CONTACTS LIST
+
 get "/contacts" do
 
   @all_contacts = Contact.all
   @contact_count = Contact.count
-  
+
   erb :contacts
 end
+
+# SINGLE CONTACT FULL LISTING
 
 get "/contacts/:id" do
 
   @contact = Contact.find(params[:id].to_i)
 
-erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+
+  end
+
 end
 
+# ABOUT ME
 get "/about" do
 
   erb :about
 end
 
+# Closes Connections beacause minirecord won't
 
 after do
   ActiveRecord::Base.connection.close

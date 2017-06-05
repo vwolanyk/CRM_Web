dad49d7ba58517979ca861a343570f5a47100de3
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-05 13:45:53 -0400

    Add COntact form, posts successfully to db - works

--- crm.rb
+++ crm.rb
@@ -49,6 +49,20 @@ get "/add_contact" do
   erb :add_contact
 end
 
+post '/add_contact' do
+
+Contact.create(
+first_name: params[:first_name],
+last_name:  params[:last_name],
+email:      params[:email],
+note:       params[:note],
+)
+
+redirect to('/contacts')
+
+
+end
+
 
 # ABOUT ME
 get "/about" do
